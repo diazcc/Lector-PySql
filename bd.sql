d73f370fc3bb934d54b793dabf17693cb4d5655c
Andres Diaz <diazccx@gmail.com>
2023-10-19 23:12:20 -0500

    Creating bd

--- bd.sql
+++ bd.sql
@@ -1,1 +1,13 @@
 CREATE DATABASE datosdb;
+
+USE datosdb;
+
+CREATE TABLE informacion (
+    codigo INT AUTO_INCREMENT,
+    nombrearchivo VARCHAR(30),
+    cantlineas INT(15),
+    cantpalabras INT(15),
+    cantcaracteres INT(15),
+    fecharegistro TIMESTAMP,
+    PRIMARY KEY (codigo)
+);
